55d55e88a48bfa1eb0ccd99ccc0436946ee1cbc7
MansAlien <mans012033945421@gmail.com>
2025-01-04 10:03:13 +0200

    add users functionality

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from app.config.database import engine, Base
-from app.posts import router
+from app.posts import router as posts_router
+from app.users import router as users_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(router.router, prefix="/api", tags=["posts"])
+app.include_router(users_router.router, prefix="/api/users", tags=["users"])
+app.include_router(posts_router.router, prefix="/api/posts", tags=["posts"])
 
 @app.get("/")
 def root():
---
+++ app/users/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from app.users import models, schemas
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
