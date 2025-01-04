be1411eebd963727435e823939dcb54f6226ff49
MansAlien <mans012033945421@gmail.com>
2025-01-04 17:22:55 +0200

    add auth functionality

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.config.database import engine, Base
 from app.posts import router as posts_router
 from app.users import router as users_router
+from app.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(auth_router.router, prefix="/api/auth", tags=["auth"])
 app.include_router(users_router.router, prefix="/api/users", tags=["users"])
 app.include_router(posts_router.router, prefix="/api/posts", tags=["posts"])
 
--- app/users/router.py
+++ app/users/router.py
@@ -1,22 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from app.auth.auth import get_current_user
 from app.config.database import get_db
 from app.users import crud, schemas
 
 router = APIRouter()
 
-@router.post("/users/", response_model=schemas.User)
+@router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db, user)
 
-@router.get("/users/", response_model=list[schemas.User])
-def read_users(db: Session = Depends(get_db)):
+@router.get("/me", response_model=schemas.User)
+def read_current_user(current_user: schemas.User = Depends(get_current_user)):
+    return current_user
+
+@router.get("/users", response_model=list[schemas.User])
+def read_users(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     return crud.get_users(db)
 
-@router.get("/users/{username}", response_model=schemas.User)
+@router.get("/{username}", response_model=schemas.User)
 def read_user(username: str, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=username)
     if not db_user:
