9984a2b3926b54a8657ddcbcd9fc539611c42ebf
MansAlien <mans012033945421@gmail.com>
2025-01-04 09:01:54 +0200

    switch to sqlite for development

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,18 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Settings(BaseSettings):
-    pg_name: str  # Ensure the names match the environment variables
-    pg_host: str
-    pg_port: str
-    pg_user: str
-    pg_password: str
-
-    @property
-    def DATABASE_URL(self) -> str:
-        return f"postgresql://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_name}?sslmode=require"
-
-    class Config:
-        env_file = ".env"  # Specify the environment file to load
-        extra = "ignore"  # Ignore extra fields in the .env file
+# class Settings(BaseSettings):
+#     pg_name: str  # Ensure the names match the environment variables
+#     pg_host: str
+#     pg_port: str
+#     pg_user: str
+#     pg_password: str
+#
+#     @property
+#     def DATABASE_URL(self) -> str:
+#         return f"postgresql://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_name}?sslmode=require"
+#
+#     class Config:
+#         env_file = ".env"  # Specify the environment file to load
+#         extra = "ignore"  # Ignore extra fields in the .env file
+#
+class Settings:
+    DATABASE_URL = "sqlite:///./data/test.db"
 
